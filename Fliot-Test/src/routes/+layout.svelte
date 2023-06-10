31acbe4295d3cd66626e72cfeb6e4cf595c2c9b5
Ryan Maxin <rsmaxin@uwaterloo.ca>
2023-06-10 09:33:09 -0400

    Added Authentication, haven't yet merged

---
+++ Fliot-Test/src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script >
+    import { onMount } from 'svelte'
+    import { auth } from '../lib/firebase/firebase'
+	import { authStore } from '../store/store';
+
+    const nonAuthRoutes = ['/'] //Only the login page for now !!UPDATE LATER!!
+
+    // On mount = on page load
+    onMount(() => {
+        console.log("Mounting")
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            const currentPath = window.location.pathname
+            if (user) {
+                if (currentPath === '/') window.location.href = '/dashboard'
+                authStore.set({email: user.email})
+                console.log(user.email)
+            }
+            else {
+                if (!nonAuthRoutes.includes(currentPath)) window.location.href = '/'
+                authStore.set({email:""})
+            }
+            return
+            
+            
+            
+        })
+    })
+</script>
+
+<h1 id = "logo"><span id = "black">Fl</span><span id= "blue">iot</span></h1>
+<div>
+    <slot/>
+</div>
+
+<style>
+    @font-face {
+        src: url(fonts/Raleway/Raleway-VariableFont_wght.ttf);
+        font-family: raleway;
+    }
+    #logo {
+        font-family: raleway;
+        font-size: 8rem;
+        padding: 0rem;
+        text-align: center;
+    }
+    #blue { 
+        color: #34ace0;
+    }
+
+</style>
---
+++ Fliot-Test/src/routes/dashboard/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { get } from "svelte/store";
+
+
+	import { authHandlers, authStore } from "../../store/store";
+	import { auth } from "$lib/firebase/firebase";
+	let email = null
+	auth.onAuthStateChanged((user) => {
+		if (user) {
+			email = user.email
+			// ...
+		} else {
+			//Shouldn't occur
+			return
+		}
+	})
+
+</script>
+{#if email}
+	<h2>Welcome, {email}</h2>
+{/if}
+
+<button on:click={authHandlers.logout}>LOGOUT</button>
